3e02d848f5dc5749616d4fa12716565b6e6a43a1
unknown <sanyapridava@mail.ru>
2024-03-17 20:12:09 +0300

    Create animation for Filelist

--- gui/src/components/ListFiles.js
+++ gui/src/components/ListFiles.js
@@ -6,46 +6,22 @@ import FileIcon from "../images/FileIcon.png";
 export default function ListFiles() {
 
     const [showfiles, Show] = useState([
-        {transition: "0.5s", opacity: "0"},
-        {transition: "1s", opacity: "0"},
-        {transition: "1.5s", opacity: "0"},
+        {transition: "0.4s", opacity: "0"},
+        {transition: "0.8s", opacity: "0"},
+        {transition: "1.2s", opacity: "0"},
     ]);
 
-    useEffect(()=>{
-        console.log("Изменилось состояние!")
-    }, [showfiles])
-
     const triangle_in_head = useRef();
-    var flag = true;
 
 
     const showList = () => {
         var triangle = triangle_in_head.current;
-
-        if (flag){
-            triangle.style.transform = "rotate(90deg)";
-
-                Show([
-                    {transition: "0.5s", opacity: "1"},
-                    {transition: "1s", opacity: "1"},
-                    {transition: "1.5s", opacity: "1"},
-                ]);
-
-        }
-        else {
-            triangle.style.transform = "rotate(0deg)";
-
-            Show([
-                {transition: "0.5s", opacity: "0"},
-                {transition: "1s", opacity: "0"},
-                {transition: "1.5s", opacity: "0"},
-            ]);
-
-        }
-
-        flag = !flag;
-
-
+        triangle.style.transform = "rotate(90deg)";
+        Show([
+            {transition: "0.4s", opacity: "1"},
+            {transition: "0.8s", opacity: "1"},
+            {transition: "1.2s", opacity: "1"},
+        ]);
     }
 
     return <div className={"ListFiles"}>
@@ -53,18 +29,16 @@ export default function ListFiles() {
             <label>Выберите программу для трансляции</label>
         </div>
 
+        {showfiles.map(item =>
+            <div className={"FileName"} style={item} onClick={()=>{
+                Show([
+                    {transition: "1.2s", opacity: "0"},
+                    {transition: "0.8s", opacity: "0"},
+                    {transition: "0.4s", opacity: "0"},
+                ]);
+            }}><img src={FileIcon}/>Program.cs</div>)}
 
-        <div>
-            <div className={"FileName"} style={{transition: showfiles[0].transition, opacity: showfiles[0].opacity}}><img
-                src={FileIcon}/>Program.cs
-            </div>
-            <div className={"FileName"} style={{transition: showfiles[1].transition, opacity: showfiles[1].opacity}}><img
-                src={FileIcon}/>main.cs
-            </div>
-            <div className={"FileName"} style={{transition: showfiles[2].transition, opacity: showfiles[2].opacity}}><img
-                src={FileIcon}/>example.cs
-            </div>
-        </div>
+
 
     </div>
 }
