1f58547784c9863184db649cc38507f930f4325f
unknown <sanyapridava@mail.ru>
2024-03-17 18:32:51 +0300

    Creating accordion

--- gui/src/components/ListFiles.js
+++ gui/src/components/ListFiles.js
@@ -1,19 +1,23 @@
 import triangle from '../images/triangle.png'
 import '../styles/ListFiles.css';
-import {useRef, useState} from "react";
-import ListProgram from "./ListFiles/Variants";
+import {useEffect, useRef, useState} from "react";
+import FileIcon from "../images/FileIcon.png";
 
-export default function ListFiles(props) {
+export default function ListFiles() {
 
     const [showfiles, Show] = useState([
-        {transition: "0.2s", opacity: "0"},
-        {transition: "0.4s", opacity: "0"},
-        {transition: "0.6s", opacity: "0"},
+        {transition: "0.5s", opacity: "0"},
+        {transition: "1s", opacity: "0"},
+        {transition: "1.5s", opacity: "0"},
     ]);
 
-    const triangle_in_head = useRef();
+    useEffect(()=>{
+        console.log("Изменилось состояние!")
+    }, [showfiles])
 
+    const triangle_in_head = useRef();
     var flag = true;
+
 
     const showList = () => {
         var triangle = triangle_in_head.current;
@@ -21,28 +25,46 @@ export default function ListFiles(props) {
         if (flag){
             triangle.style.transform = "rotate(90deg)";
 
+                Show([
+                    {transition: "0.5s", opacity: "1"},
+                    {transition: "1s", opacity: "1"},
+                    {transition: "1.5s", opacity: "1"},
+                ]);
+
         }
         else {
             triangle.style.transform = "rotate(0deg)";
+
+            Show([
+                {transition: "0.5s", opacity: "0"},
+                {transition: "1s", opacity: "0"},
+                {transition: "1.5s", opacity: "0"},
+            ]);
+
         }
 
-        var properties = showfiles;
-        for (let i=0; i < properties.length; i++) {
-            if (flag) properties[i].opacity = "1";
-            else properties[i].opacity = "0";
-        }
-
-        //console.log(properties[0].opacity);
-        Show(properties);
         flag = !flag;
+
+
     }
 
-
     return <div className={"ListFiles"}>
-        <div id={"head_list_program"}  onClick={showList}> <img src={triangle} ref={triangle_in_head}></img>
+        <div id={"head_list_program"} onClick={showList}><img src={triangle} ref={triangle_in_head}></img>
             <label>Выберите программу для трансляции</label>
         </div>
-        <ListProgram data={showfiles}/>
+
+
+        <div>
+            <div className={"FileName"} style={{transition: showfiles[0].transition, opacity: showfiles[0].opacity}}><img
+                src={FileIcon}/>Program.cs
+            </div>
+            <div className={"FileName"} style={{transition: showfiles[1].transition, opacity: showfiles[1].opacity}}><img
+                src={FileIcon}/>main.cs
+            </div>
+            <div className={"FileName"} style={{transition: showfiles[2].transition, opacity: showfiles[2].opacity}}><img
+                src={FileIcon}/>example.cs
+            </div>
+        </div>
 
     </div>
 }
